0c0e3282a210dfe384647e75693c401eee669e13
castaneai <castaneai@castaneai.net>
2018-07-12 00:28:22 +0900

    Update test code

--- test.php
+++ test.php
@@ -1,14 +1,18 @@
 <?php
+echo '---- php version: ' . phpversion() . PHP_EOL;
+
+echo PHP_EOL . PHP_EOL;
+
 date_default_timezone_set('Asia/Tokyo');
 echo '------ set default timezone to ' . date_default_timezone_get() . PHP_EOL;
 $t = new DateTime();
+var_dump($t->format('e T P'));
 
 echo PHP_EOL . PHP_EOL;
 
 echo '------ set timezone \'UTC\'' . PHP_EOL;
 $tz = new DateTimeZone('UTC');
 $t->setTimeZone($tz);
-echo '------ show timezone info' . PHP_EOL;
 var_dump($t->format('e T P'));
 
 echo PHP_EOL . PHP_EOL;
@@ -16,5 +20,4 @@ echo PHP_EOL . PHP_EOL;
 echo '------ set timezone \'utc\'' . PHP_EOL;
 $tz2 = new DateTimeZone('utc');
 $t->setTimeZone($tz2);
-echo '------ show timezone info' . PHP_EOL;
 var_dump($t->format('e T P'));
